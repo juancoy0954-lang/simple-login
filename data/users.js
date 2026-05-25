560c29fbceead87e3fc797b6df71b13812072c1a
zionyx <juancoy0954@gmail.com>
2026-05-25 22:14:06 +0800

    Create users.js

---
+++ data/users.js
@@ -0,0 +1,6 @@
+export const MOCK_USERS = [
+  { id: 1, email: "admin@gmail.com", password: "Admin123", role: "admin", name: "Admin" },
+  { id: 2, email: "user@gmail.com",  password: "User123",  role: "user",  name: "User Biasa" },
+];
+
+export const MAX_ATTEMPTS = 3;
